7359be8acaaebff4e67dbf60800c93426d7747b7
hoang4963 <hoangth.finbox@gmail.com>
2022-07-31 11:46:00 +0700

    ve duoc khoi gach tren bang

---
+++ js.js
@@ -0,0 +1,238 @@
+// khai bao bien co dinh
+const ROWS = 20;
+const COLS = 10;
+const BLOCK_SIZE = 30;
+const COLOR_MAPPING = [
+    'red',
+    'green',
+    'blue',
+    'yellow',
+    'purple',
+    'pink',
+    'black',
+    'white'
+]
+const BRICK_LAYOUT = [
+    [
+        [
+            [1, 7, 7],
+            [1, 1, 1],
+            [7, 7, 7],
+        ],
+        [
+            [7, 1, 1],
+            [7, 1, 7],
+            [7, 1, 7],
+        ],
+        [
+            [7, 7, 7],
+            [1, 1, 1],
+            [7, 7, 1],
+        ],
+        [
+            [7, 1, 7],
+            [7, 1, 7],
+            [1, 1, 7],
+        ],
+    ],
+    [
+        [
+            [7, 1, 7],
+            [7, 1, 7],
+            [7, 1, 1],
+        ],
+        [
+            [7, 7, 7],
+            [1, 1, 1],
+            [1, 7, 7],
+        ],
+        [
+            [1, 1, 7],
+            [7, 1, 7],
+            [7, 1, 7],
+        ],
+        [
+            [7, 7, 1],
+            [1, 1, 1],
+            [7, 7, 7],
+        ],
+    ],
+    [
+        [
+            [1, 7, 7],
+            [1, 1, 7],
+            [7, 1, 7],
+        ],
+        [
+            [7, 1, 1],
+            [1, 1, 7],
+            [7, 7, 7],
+        ],
+        [
+            [7, 1, 7],
+            [7, 1, 1],
+            [7, 7, 1],
+        ],
+        [
+            [7, 7, 7],
+            [7, 1, 1],
+            [1, 1, 7],
+        ],
+    ],
+    [
+        [
+            [7, 1, 7],
+            [1, 1, 7],
+            [1, 7, 7],
+        ],
+        [
+            [1, 1, 7],
+            [7, 1, 1],
+            [7, 7, 7],
+        ],
+        [
+            [7, 7, 1],
+            [7, 1, 1],
+            [7, 1, 7],
+        ],
+        [
+            [7, 7, 7],
+            [1, 1, 7],
+            [7, 1, 1],
+        ],
+    ],
+    [
+        [
+            [7, 7, 7, 7],
+            [1, 1, 1, 1],
+            [7, 7, 7, 7],
+            [7, 7, 7, 7],
+        ],
+        [
+            [7, 7, 1, 7],
+            [7, 7, 1, 7],
+            [7, 7, 1, 7],
+            [7, 7, 1, 7],
+        ],
+        [
+            [7, 7, 7, 7],
+            [7, 7, 7, 7],
+            [1, 1, 1, 1],
+            [7, 7, 7, 7],
+        ],
+        [
+            [7, 1, 7, 7],
+            [7, 1, 7, 7],
+            [7, 1, 7, 7],
+            [7, 1, 7, 7],
+        ],
+    ],
+    [
+        [
+            [7, 7, 7, 7],
+            [7, 1, 1, 7],
+            [7, 1, 1, 7],
+            [7, 7, 7, 7],
+        ],
+        [
+            [7, 7, 7, 7],
+            [7, 1, 1, 7],
+            [7, 1, 1, 7],
+            [7, 7, 7, 7],
+        ],
+        [
+            [7, 7, 7, 7],
+            [7, 1, 1, 7],
+            [7, 1, 1, 7],
+            [7, 7, 7, 7],
+        ],
+        [
+            [7, 7, 7, 7],
+            [7, 1, 1, 7],
+            [7, 1, 1, 7],
+            [7, 7, 7, 7],
+        ],
+    ],
+    [
+        [
+            [7, 1, 7],
+            [1, 1, 1],
+            [7, 7, 7],
+        ],
+        [
+            [7, 1, 7],
+            [7, 1, 1],
+            [7, 1, 7],
+        ],
+        [
+            [7, 7, 7],
+            [1, 1, 1],
+            [7, 1, 7],
+        ],
+        [
+            [7, 1, 7],
+            [1, 1, 7],
+            [7, 1, 7],
+        ],
+    ],
+];
+// hinh dang cua cac brick luu trong 1 mang 3 chieu
+const WHITE_COLOR_ID = 7;
+const canvas = document.getElementById('board');
+const ctx = canvas.getContext('2d');
+// ve canvas
+ctx.canvas.width = COLS*BLOCK_SIZE;
+ctx.canvas.height = ROWS* BLOCK_SIZE;
+
+class Board {
+    constructor(ctx) {
+        this.ctx = ctx;
+        this.grid = this.generateWhiteBoard();
+    }
+    generateWhiteBoard() {
+        return Array.from({length: ROWS}, () => Array(COLS).fill(WHITE_COLOR_ID));
+        // bang co 20 hang moi hang 10 cot va co id = 7
+    }
+    drawCell(xAxis, yAxis, colorId) {
+        this.ctx.fillStyle = COLOR_MAPPING[colorId] || COLOR_MAPPING[WHITE_COLOR_ID];
+        this.ctx.fillRect(xAxis * BLOCK_SIZE, yAxis * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+        this.ctx.fillStyle = 'black';
+        this.ctx.strokeRect(xAxis * BLOCK_SIZE, yAxis * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+      // ve tung o trong game
+    }
+    drawBoard() {
+        for (let row = 0; row < this.grid.length; row++){
+            for (let col = 0; col < this.grid[0].length; col++) {
+                this.drawCell(col,row, WHITE_COLOR_ID)
+            }
+        }
+    }
+}
+class Brick {
+    constructor(id) {
+        this.id = id;
+        this.layout = BRICK_LAYOUT[id];
+        this.activeIndex = 0;
+        this.colPos = 3;
+        this.rowPos = 5;
+    }
+    draw() {
+        for (let row = 0; row < this.layout[this.activeIndex].length; row++ ){
+            for (let col =0; col < this.layout[this.activeIndex][0].length; col++){
+                if( this.layout[this.activeIndex][row][col] !== WHITE_COLOR_ID){
+                    board.drawCell(col + this.colPos,row + this.rowPos,this.id);
+                }
+            }
+        }
+    }
+    moveLeft() {
+        this.colPos--;
+        this.draw();
+    }
+}
+let board = new Board(ctx);
+// console.log(board) ve thu bang
+board.drawBoard()
+let brick = new Brick(0);
+brick.draw()
+// ve thu 1 hinh
