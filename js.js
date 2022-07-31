8bf5bfa3f32cd5293ce8b4f5b756410f49206900
hoang4963 <hoangth.finbox@gmail.com>
2022-07-31 13:11:11 +0700

    gan nhu hoan thanh

--- js.js
+++ js.js
@@ -194,6 +194,15 @@ class Board {
     constructor(ctx) {
         this.ctx = ctx;
         this.grid = this.generateWhiteBoard();
+        this.score = 0;
+        this.gameOver = false;
+        this.isPlaying = false;
+    }
+    reset() {
+        this.score = 0;
+        this.grid = this.generateWhiteBoard();
+        this.gameOver = false;
+        this.drawBoard()
     }
     generateWhiteBoard() {
         return Array.from({length: ROWS}, () => Array(COLS).fill(WHITE_COLOR_ID));
@@ -213,6 +222,26 @@ class Board {
             }
         }
     }
+    handleCompleteRows(){
+       const lastestGrid = board.grid.filter((row) => {
+            return row.some(col => col === WHITE_COLOR_ID);
+        });
+       const newScore = ROWS - lastestGrid.length;
+       const newRows = Array.from({length: newScore}, () => Array(COLS).fill(WHITE_COLOR_ID));
+       if (newScore) {
+           board.grid = [...newRows, ...lastestGrid];
+           this.handleScore(newScore * 10)
+       }
+    }
+    handleScore(newScore) {
+        this.score += newScore
+        document.getElementById('score').innerHTML = this.score;
+    }
+    handleGameOver() {
+        this.gameOver = true;
+        board.isPlaying = false;
+        alert('Con Gà !!!')
+    }
 }
 class Brick {
     constructor(id) {
@@ -220,7 +249,7 @@ class Brick {
         this.layout = BRICK_LAYOUT[id];
         this.activeIndex = 0;
         this.colPos = 3;
-        this.rowPos = 5;
+        this.rowPos = -2;
     }
     draw() {
         for (let row = 0; row < this.layout[this.activeIndex].length; row++ ){
@@ -262,7 +291,8 @@ class Brick {
             return
         }
         this.handleLanded();
-        generateNewBrick();
+        if (!board.gameOver){
+        generateNewBrick();}
     }
     rotate() {
         if (!this.checkCollision(this.rowPos + 1, this.colPos,this.layout[(this.activeIndex + 1) % 4]  )) {
@@ -275,7 +305,7 @@ class Brick {
     checkCollision(nextRow,nextCol,nextLayout){
         for (let row = 0; row < nextLayout.length; row++ ){
             for (let col =0; col < nextLayout[0].length; col++){
-                if( nextLayout[row][col] !== WHITE_COLOR_ID){
+                if( nextLayout[row][col] !== WHITE_COLOR_ID && nextRow >= 0){
                     if ((col + nextCol >= COLS) ||
                         (row + nextRow >= ROWS) ||
                         (col + nextCol < 0) ||
@@ -287,6 +317,10 @@ class Brick {
     return false;
     }
     handleLanded() {
+        if ( this.rowPos <=0) {
+            board.handleGameOver();
+            return;
+        }
         for (let row = 0; row < this.layout[this.activeIndex].length; row++ ){
             for (let col =0; col < this.layout[this.activeIndex][0].length; col++){
                 if( this.layout[this.activeIndex][row][col] !== WHITE_COLOR_ID){
@@ -294,8 +328,10 @@ class Brick {
                 }
             }
         }
+        board.handleCompleteRows()
         board.drawBoard();
     }
+
 }
 function generateNewBrick() {
     brick = new Brick(Math.floor(Math.random()*10) % 7)
@@ -305,14 +341,27 @@ let board = new Board(ctx);
 // console.log(board) ve thu bang
 board.drawBoard()
 generateNewBrick();
-brick.draw()
+document.getElementById('play').addEventListener('click', () => {
+    board.reset();
+    generateNewBrick();
+    board.isPlaying = true;
+    const refresh = setInterval(() => {
+        if (!board.gameOver) {
+            brick.moveDown();
+        } else {
+            clearInterval(refresh);
+        }
+    }, 1000);
+})
+
+
+// brick.draw()
 // ve thu 1 hinh
 
-setInterval(() => {
-    brick.moveDown();
-}, 1000)
+
 
 document.addEventListener('keydown', (e) => {
+    if (!board.gameOver && board.isPlaying){
     switch (e.code){
         case KEYCODE.LEFT:
             brick.moveLeft();
@@ -322,11 +371,12 @@ document.addEventListener('keydown', (e) => {
             break;
         case KEYCODE.DOWN:
             brick.moveDown();
+            brick.moveDown();
             break;
         case KEYCODE.UP:
             brick.rotate();
             break;
         default:
-            break;
+            break;}
     }
 })
